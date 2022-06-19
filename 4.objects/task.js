e44817c53cfe80caebb1be47663ff479d9c623b4
Evgeniy Kvasov <E_kvasov@inbox.ru>
2022-06-20 00:34:23 +0500

    Первая версия д/з

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [];
+    this.marks[0] = mark;
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...args) {
+  if(this.marks === undefined){
+    this.marks = [];
+    args.forEach((item, idx) => this.marks[idx] = item);
+  } else {
+  args.forEach((item) => this.marks.push(item));
+  }
+}
+
+Student.prototype.getAverage = function () {
+  const average = this.marks.reduce((acc, item, idx, arr) => {
+    if (idx === arr.length - 1) {
+      return (acc + item) / arr.length;
+    } else {
+      return acc + item;
+    }
+  });
+  return average;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
